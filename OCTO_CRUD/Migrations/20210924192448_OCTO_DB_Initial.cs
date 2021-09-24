9bed509f32c097f0e8a8866d3d2fc2bb5436483c
Edson Soares <edsondearaujofga@gmail.com>
2021-09-24 17:16:34 -0300

    #3 Created Migrations ClientContext

---
+++ OCTO_CRUD/Migrations/20210924192448_OCTO_DB_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OCTO_CRUD.Migrations
+{
+    public partial class OCTO_DB_Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    IdClient = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    ZipCode = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Cnpj = table.Column<string>(type: "varchar(50)", nullable: false),
+                    CellPhone = table.Column<string>(type: "varchar(50)", nullable: true),
+                    HomePhone = table.Column<string>(type: "varchar(50)", nullable: true),
+                    CommercialPhone = table.Column<string>(type: "varchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.IdClient);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
